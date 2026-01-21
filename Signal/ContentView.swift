589ccc727cd624ab583f5b58f85a527154682d0f
Oliverknowledge <ogsknowledge@gmail.com>
2026-01-21 09:12:17 +0000

    Initial Commit

---
+++ Signal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Signal
+//
+//  Created by Oliver Stevenson on 21/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Signal/SignalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignalApp.swift
+//  Signal
+//
+//  Created by Oliver Stevenson on 21/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct SignalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
